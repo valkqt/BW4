1eecbae3449756803b8994207ca5aac70ce88961
marcod08 <marcodimpirma91@gmai.com>
2024-02-19 18:39:32 +0100

    prima stesura di details

--- Details.aspx
+++ Details.aspx
@@ -2,6 +2,30 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col">
+                <asp:Image ID="imgProduct" runat="server" />
+            </div>
+            <div class="col">
+                <asp:Label ID="lblCategory" runat="server" />
+                <br />
+                <asp:Label ID="lblBrand" runat="server" />
+                <asp:Label ID="lblTitle" runat="server" />
+                <br />
+                <asp:Label ID="lblPrice" runat="server" />
+                <br />
+                <asp:Label ID="lblDescription" runat="server" />
+                <br />
+                <asp:TextBox ID="txtQuantity" runat="server" Text="1" />
+                <asp:Button ID="btnAddToCart" runat="server" Text="Add to Cart" OnClick="btnAddToCart_Click" />
+            </div>
+        </div>
+
+    </div>
+
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
 </asp:Content>
--- Details.aspx.cs
+++ Details.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,46 @@ namespace BW4
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            {
+                if (!IsPostBack)
+                {
+                    // id di test 
+                    int productId = 1;
+                    // id dinamico
+                    // int productId = Convert.ToInt32(Request.QueryString["productId"]);
 
+                    string connectionString = ConfigurationManager.ConnectionStrings["Products"].ConnectionString;
+
+                    string query = "SELECT brand, category, title, price, description, images FROM Products WHERE id = @id";
+
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        using (SqlCommand command = new SqlCommand(query, connection))
+                        {
+                            command.Parameters.AddWithValue("@id", productId);
+                            connection.Open();
+                            SqlDataReader reader = command.ExecuteReader();
+                            if (reader.Read())
+                            {
+                                string upperCategory = reader["category"].ToString();
+
+                                lblCategory.Text = char.ToUpper(upperCategory[0]) + upperCategory.Substring(1);
+                                lblBrand.Text = reader["brand"].ToString();
+                                lblTitle.Text = reader["title"].ToString();
+                                lblPrice.Text = "Price: $" + reader["price"].ToString();
+                                lblDescription.Text = reader["description"].ToString();
+                                imgProduct.ImageUrl = reader["images"].ToString();
+                            }
+                            reader.Close();
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void btnAddToCart_Click(object sender, EventArgs e)
+        {
+           //funzione click sul bottone add to card
         }
     }
 }
