9ed5cf84c5496a1b31d77533cfd137626cf6c2cd
Valk <bznand@gmail.com>
2024-02-22 11:02:54 +0100

    qqq

--- Home.aspx.designer.cs
+++ Home.aspx.designer.cs
@@ -14,14 +14,7 @@ namespace BW4
     public partial class Home
     {
 
-        /// <summary>
-        /// AdminButton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button AdminButton;
+
 
         /// <summary>
         /// NewArrivalsRepeater control.
