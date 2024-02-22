d720a316ff61e770b5f335a61fb1cfcc7ff52ef4
MarcoV98 <148749261+MarcoV98@users.noreply.github.com>
2024-02-22 17:11:02 +0100

    checkout finale

--- Checkout.aspx.cs
+++ Checkout.aspx.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text.RegularExpressions;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace BW4
 {
@@ -11,7 +10,92 @@ namespace BW4
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                ShowProductsInSummary();
+            }
+        }
+        private void ShowProductsInSummary()
+        {
+            List<Product> cart = Session["Cart"] as List<Product>;
 
+            if (cart == null || cart.Count == 0)
+            {
+                rptProdottiRiepilogo.Visible = false;
+                lblTotaleRiepilogo.Text = "Your cart is empty!";
+            }
+            else
+            {
+                rptProdottiRiepilogo.DataSource = cart;
+                rptProdottiRiepilogo.DataBind();
+
+                decimal total = cart.Sum(p => p.Price * p.Quantity);
+                decimal shoppingFee = new Random().Next(0, 6); // Numero casuale da 0 a 5 per la shopping fee
+
+                lblTotaleRiepilogo.Text = total.ToString("0.00");
+                lblShippingFee.Text = shoppingFee.ToString("0.00");
+
+                // Calcola e mostra il totale (subtotal + shopping fee)
+                decimal grandTotal = total + shoppingFee;
+                lblGrandTotal.Text = grandTotal.ToString("0.00");
+            }
+        }
+
+
+        protected void btnPlaceOrder_Click(object sender, EventArgs e)
+        {
+
+            if (!IsValidEmail(txtUsernameEmail.Text.Trim()))
+            {
+                ShowErrorMessage("Invalid email.");
+                return;
+            }
+
+            if (!IsValidName(txtFirstName.Text) || !IsValidName(txtLastName.Text) || !IsValidName(txtCity.Text))
+            {
+                ShowErrorMessage("First name, last name or city are invalid.");
+                return;
+            }
+
+            int cap;
+            if (!int.TryParse(txtZipCode.Text, out cap))
+            {
+                ShowErrorMessage("Invalid Zip code.");
+                return;
+            }
+
+            string telefono = txtPhone.Text.Trim();
+            if (!IsValidPhoneNumber(telefono))
+            {
+                ShowErrorMessage("Invalid Phone number.");
+                return;
+            }
+
+            Response.Redirect("/Home.aspx");
+        }
+
+        private void ShowErrorMessage(string message)
+        {
+            lblMessaggioErrore.Text = message;
+            lblMessaggioErrore.Visible = true;
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            return Regex.IsMatch(email, @"\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b", RegexOptions.IgnoreCase);
+        }
+
+        private bool IsValidName(string name)
+        {
+            return !string.IsNullOrWhiteSpace(name);
+        }
+
+        private bool IsValidPhoneNumber(string phoneNumber)
+        {
+            return Regex.IsMatch(phoneNumber, @"^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d$");
         }
     }
 }
+
+
+
